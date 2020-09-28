4bc8021c7b140c161cf8fca971cfb17a916b62b9
han1475 <me@han1475.com>
2020-09-28 23:15:45 +0800

    feat(highlight): add code highlight

--- assets/js/cupcake.js
+++ assets/js/cupcake.js
@@ -1,3 +1,6 @@
+/**
+ * readmore
+ */
 function readmore(obj){
     if (obj.value=="readmore"){
         obj.parentNode.getElementsByClassName('post-summary')[0].style.display="none";
@@ -11,3 +14,38 @@ function readmore(obj){
         obj.value="readmore";
     }
 }
+
+/**
+ * highlight
+ */
+var highlight = function () {
+    document.querySelectorAll('.chroma>table')
+      .forEach((element) => {
+        const sub = element.querySelector('code[data-lang]');
+        if (sub !== null) {
+          element.setAttribute('data-lang', mapLang(sub.getAttribute('data-lang')));
+        }
+    });
+}
+function mapLang(name) {
+    return {
+      coffeescript: 'CoffeeScript',
+      cpp: 'C++',
+      cs: 'C#',
+      css: 'CSS',
+      html: 'HTML',
+      http: 'HTTP',
+      js: 'JavaScript',
+      json: 'JSON',
+      objectivec: 'Objective-C',
+      php: 'PHP',
+      sql: 'SQL',
+      toml: 'TOML',
+      ts: 'TypeScript',
+      typescript: 'TypeScript',
+      xml: 'XML',
+      yaml: 'YAML',
+    }[name] || name;
+  }
+highlight();
+
